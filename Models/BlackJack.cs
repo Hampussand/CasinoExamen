2624dbe181975b50b156fc644f132f939fe41bd9
Oliver Mineur <olivermineur@gmail.com>
2025-01-30 10:48:19 +0100

    as

--- Models/BlackJack.cs
+++ Models/BlackJack.cs
@@ -8,20 +8,32 @@ namespace MauiApp2.Models
 {
     internal class BlackJack
     {
-        public List<Card> Deck { get;
+        public List<Card> Deck 
+        {
+            get { return Deck; }
             set
             {
-                foreach(Card card in Enum.GetValues(typeof(Card.Suit)))
+                foreach (Card card in Enum.GetValues(typeof(Card.Suit)))
                 {
-                    foreach()
+                    for (int i = 0; i < 13; i++)
+                    {
+                        Deck.Add();
+                    }
                 }
-            } 
-                }
+            }
+        }
     }
     class Card
     {
         public enum Suit { Hearts, Diamonds, Clubs, Spades };
-        public int Rank;
+        public int Rank
+        {
+            get { return Rank; }
+            set
+            {
+                Rank = value;
+            }
+        }
     }
 
 
